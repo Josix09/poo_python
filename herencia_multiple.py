ddffacde429306a544df89e88c1f749b7a585b59
Josix09 <jorgesilva@colegioguanenta.edu.co>
2025-03-21 10:06:00 -0500

    archivo actualizado

---
+++ herencia_multiple.py
@@ -0,0 +1,10 @@
+# la herencia multiple  se puee ver de varias formas
+#una clase puede presentar herencia multiple
+#al heredar de mas de una clase
+
+class Clase1:
+    pass
+class Clase2:
+    pass
+class Clase3(Clase1, Clase2):
+    pass
